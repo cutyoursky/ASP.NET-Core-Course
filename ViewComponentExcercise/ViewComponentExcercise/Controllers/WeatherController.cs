80191e3abab597a4cc0a40662af4c596bde98351
cutyoursky <kotarskiigor.business@gmail.com>
2024-01-31 18:37:07 +0100

    kolejne zadanko

---
+++ ViewComponentExcercise/ViewComponentExcercise/Controllers/WeatherController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ViewComponentExcercise.Models;
+
+namespace ViewComponentExcercise.Controllers
+{
+    public class WeatherController : Controller
+    {
+        private List<City> cities = new List<City> {
+        new City () { CityUniqueCode = "LDN", CityName = "London", DateAndTime = Convert.ToDateTime( "2030-01-01 8:00"),  TemperatureFahrenheit = 33 },
+        new City () { CityUniqueCode = "NYC", CityName = "London", DateAndTime = Convert.ToDateTime( "2030-01-01 3:00"),  TemperatureFahrenheit = 60 },
+        new City () { CityUniqueCode = "PAR", CityName = "Paris", DateAndTime = Convert.ToDateTime("2030-01-01 9:00"),  TemperatureFahrenheit = 82 }
+        };
+
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View(cities);
+        }
+
+        [Route("/weather/{cityCode?}")]
+        public IActionResult City(string? cityCode)
+        {
+            if (string.IsNullOrEmpty(cityCode))
+            {
+                return View();
+            }
+
+            City? city = cities.Where(city => city.CityUniqueCode == cityCode).FirstOrDefault();
+            return View(city);
+        }
+    }
+}
---
+++ ViewComponentExcercise/ViewComponentExcercise/ViewComponents/CityViewComponent.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ViewComponentExcercise.Models;
+
+namespace ViewComponentExcercise.ViewComponents
+{
+    public class CityViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(City city)
+        {
+            ViewBag.Color = GetColorByTemperature(city.TemperatureFahrenheit);
+
+            return View(city);
+        }
+
+        string GetColorByTemperature(int temperature)
+        {
+            if (temperature < 44)
+            {
+                return "blue-background";
+            }
+            else if (temperature >= 44 && temperature <= 75)
+            {
+                return "green-background";
+            }
+            else
+            {
+                return "orange-background";
+            }
+        }
+    }
+}
